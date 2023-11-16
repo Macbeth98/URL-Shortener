f90bbb1f68c84cb49449a30fdf0b2538b764fda5
Macbeth98 <manichandra.teja@gmail.com>
2023-11-15 20:20:58 -0500

    doc: Added the dev document reasoning why ServiceContainer class exists

--- src/modules/containers/service.container.ts
+++ src/modules/containers/service.container.ts
@@ -7,6 +7,19 @@ import { AuthModule } from '../auth/auth.module';
 import { UrlService } from '../url/url.service';
 import { UrlModule } from '../url/url.module';
 
+/**
+ * @dev This class is used to store all the services that are used in the application.
+ * @dev This class is created to avoid the service layer dependency on Fastify Framework, especially in this case, the FastifyInstance.
+ * @dev Otherwise to use Fastify Plugins such as I would have to import FastifyInstance in the service layer, which I don't want to.
+ * @dev I tried designing the application in such a way that the service layer is independent of the framework. I can easily swap out Fastify with Express or Koa.
+ *      Just by changing the Routing layer and this ServiceContainer class.
+ * @dev This class is a singleton class, so that the services are instantiated only once. And those services are instantiated in its own Module.
+ * @dev This class also serves as a dependency injection container and also to avoid any circular dependency.
+ *      Instead of being tightly coupled with the fastify instance, the service layer now uses this ServiceContainer class to access the services.
+ * @dev This class is initialized when the fastify instance is created and is decorated to fastify to access in the Routing layer.
+ *      So that the Routing layer can access the services and do service layer dependency injection to the controllers.
+ */
+
 export class ServiceContainer {
   private initialUrlCounter = 3844;
 
